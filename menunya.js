942550a3ed923c2288b8a6ef3baba81ad7387820
Kai0071 <achintya0071@gmail.com>
2023-07-20 07:57:43 +0530

    noice

---
+++ menunya.js
@@ -0,0 +1,897 @@
+const chalk = require('chalk')
+const fs = require('fs')
+
+global.menunya = (pushname, prefix, hituet) =>{
+	return `╭─❒ 「 *INFO MENU* 」
+┃
+┃▢ .owner  
+┃▢ .sc  
+┃▢ .ping  
+┃▢ .donate  
+┃▢ .tqto  
+┃▢ .sewa  
+┃▢ .q  
+┃▢ .jasrun  
+┃▢ .sewa  
+┃▢ .buyprem  
+┃▢ .runtime  
+┃▢ .groupbot  
+┃▢ .developer  
+┃
+╰❒ 
+
+╭─❒ 「 *OWNER MENU* 」
+┃
+┃▢ .addprem  
+┃▢ .delprem  
+┃▢ .listprem  
+┃▢ .block  
+┃▢ .unblock  
+┃▢ .public  
+┃▢ .self  
+┃▢ .out  
+┃▢ .kosong  
+┃▢ .join  
+┃▢ .addsewa  
+┃▢ .delsewa  
+┃▢ .setnamabot  
+┃▢ .setbiobot  
+┃▢ .broadcastvid  
+┃▢ .bctext  
+┃▢ .bcimage  
+┃▢ .pushkon
+┃▢ .pushkonid
+┃▢ .anticall [on/off]
+┃▢ .setexif
+┃▢ .idgrub
+┃▢ .idgc
+┃▢ .jadibot
+┃▢ .listjadibot
+┃▢ .restartbot
+┃▢ .creategc
+┃▢ .banned
+┃▢ .listbanned
+┃▢ .unbanned
+┃▢ .autoread
+┃▢ .autoblok212 [on/off]
+┃▢ .getcase
+┃▢ .setppbot
+┃▢ .pppanjang
+┃▢ .addowner
+┃▢ .delowner
+┃▢ .setbot
+┃▢ .delsetbot
+┃
+╰❒
+
+╭─❒ 「 *DATABASE MENU* 」
+┃
+┃▢ .addvn
+┃▢ .listvn
+┃▢ .delvn
+┃▢ .addzip
+┃▢ .delzip
+┃▢ .listzip
+┃▢ .yozip
+┃▢ .sendzip
+┃▢ .addapk
+┃▢ .delapk
+┃▢ .listapk
+┃▢ .sendapk
+┃▢ .addpdf
+┃▢ .listpdf
+┃▢ .sendpdf
+┃▢ .yopdf
+┃▢ .sendpdf
+┃
+╰❒
+
+╭─❒ 「  *STORE MENU*  」
+┃
+┃▢ .list  
+┃▢ .addlist  
+┃▢ .updatelist  
+┃▢ .dellist  
+┃▢ .jeda  
+┃▢ .tambah  
+┃▢ .kurang  
+┃▢ .kali  
+┃▢ .bagi  
+┃▢ .setproses  
+┃▢ .changeproses  
+┃▢ .delsetproses  
+┃▢ .setdone  
+┃▢ .changedone  
+┃▢ .delsetdone  
+┃▢ .proses  
+┃▢ .done  
+┃▢ .setwelcome  
+┃▢ .changewelcome  
+┃▢ .delsetwelcome  
+┃▢ .setleft  
+┃▢ .changeleft  
+┃▢ .delsetleft  
+┃▢ .opentime
+┃▢ .closetime
+┃
+╰❒
+
+╭─❒ 「 *GROUP MENU* 」 
+┃
+┃▢ .antiwame  [on/off]
+┃▢ .antiwame2  [on/off]
+┃▢ .antilink  [on/off]
+┃▢ .antilink2  [on/off]
+┃▢ .welcome  [on/off]
+┃▢ .goodbye  [on/off]
+┃▢ .group [open/close]
+┃▢ .group [close/open]
+┃▢ .hidetag  
+┃▢ .kick  
+┃▢ .linkgc  
+┃▢ .resetlinkgc  
+┃▢ .delete  
+┃▢ .listonline  
+┃▢ .setnamegc  
+┃▢ .setdesc  
+┃▢ .listsewa  
+┃▢ .add  
+┃▢ .promote  
+┃▢ .demote  
+┃▢ .antilinkall [on/off]
+┃▢ .sendlinkgc  
+┃▢ .tagall  
+┃▢ .totag
+┃▢ .mute [on/off]
+┃▢ .nsfw [on/off]
+┃▢ .setppgroup
+┃
+╰❒
+
+╭─❒ 「 *SEARCH MENU* 」 
+┃
+┃▢ .ytsearch  
+┃▢ .google  
+┃▢ .cariresep
+┃▢ .bacaresep
+┃▢ .happymod
+┃▢ .ssweb
+┃▢ .grupwa
+┃▢ .translate
+┃▢ .listbahasa
+┃▢ .liriklagu
+┃
+╰❒
+
+╭─❒ 「 *DOWNLOAD MENU* 」
+┃ 
+┃▢ .tiktok  
+┃▢ .tiktokvn  
+┃▢ .tiktokaudio  
+┃▢ .tiktok2
+┃▢ .tiktokaudio2
+┃▢ .spotify  
+┃▢ .ytmp3  
+┃▢ .ytmp4  
+┃▢ .play  
+┃▢ .playmp3  
+┃▢ .playmp4  
+┃▢ .tomp4
+┃▢ .toaudio
+┃▢ .tomp3
+┃▢ .tovn
+┃▢ .togif
+┃▢ .igdl  
+┃▢ .igvideo  
+┃▢ .instavid
+┃▢ .igreels
+┃▢ .fbdl
+┃▢ .facebook
+┃▢ .gitclone
+┃▢ .twiter
+┃
+╰❒
+
+╭─❒ 「 *ASUPAN MENU* 」
+┃
+┃▢ .tiktokkayes  
+┃▢ .tiktokgirl  
+┃▢ .tiktokghea  
+┃▢ .tiktokbocil  
+┃▢ .tiktokukhty  
+┃▢ .tiktoksantuy  
+┃▢ .tiktokpanrika  
+┃▢ .tiktoknotnot  
+┃▢ .tiktokgabagtha  
+┃
+╰❒
+
+╭─❒ 「 *ASUPAN* *2* *MENU* 」
+┃
+┃▢ .chika
+┃▢ .delvira
+┃▢ .ayu
+┃▢ .bunga
+┃▢ .aura
+┃▢ .nisa
+┃▢ .ziva
+┃▢ .yana
+┃▢ .viona
+┃▢ .syania
+┃▢ .riri
+┃▢ .syifa
+┃▢ .mama_gina
+┃▢ .alcakenya
+┃▢ .mangayutri
+┃▢ .rikagusriani
+┃▢ .asupan
+┃▢ .bocil
+┃▢ .geayubi
+┃▢ .santuy
+┃▢ .ukhty
+┃▢ .syifa
+┃
+╰❒
+
+╭─❒ 「 *OTHER MENU* 」 
+┃
+┃▢ .afk
+┃▢ .bot
+┃▢ .sapa
+┃▢ .remini
+┃▢ .getpic
+┃▢ .gtts
+┃▢ .
+┃
+╰❒
+
+╭─❒ 「 *RANDOM STICKER* 」 
+┃
+┃▢ .patrick  
+┃▢ .popoci  
+┃▢ .sponsbob  
+┃▢ .kawan-sponsbob  
+┃▢ .awoawo  
+┃▢ .chat  
+┃▢ .benedict  
+┃▢ .dbfly  
+┃▢ .dino-kuning  
+┃▢ .doge  
+┃▢ .gojosatoru  
+┃▢ .hope-boy  
+┃▢ .jisoo  
+┃▢ .kr-robot  
+┃▢ .kucing   
+┃▢ .lonte  
+┃▢ .manusia_lidi  
+┃▢ .menjamet  
+┃▢ .meow  
+┃▢ .nicholas  
+┃▢ .tyni  
+┃
+╰❒
+
+╭─❒ 「 *STICKER MENU* 」 
+┃
+┃▢ .sticker  
+┃▢ .toimage  
+┃▢ .pinterest  
+┃▢ .ttp  
+┃▢ .smeme  
+┃▢ .attp  
+┃▢ .emojimix  
+┃▢ .wm  
+┃▢ .qc
+┃▢ .snobg
+┃
+╰❒
+
+╭─❒ 「 *Anonymous MENU* 」 
+┃
+┃▢ .menfess
+┃
+╰❒
+
+╭─❒ 「 *Ai Menu* 」 
+┃
+┃▢ .ai
+┃▢ .chatgpt  
+┃▢ .aiimg
+┃
+╰❒
+
+╭─❒ 「 *STALKER MENU* 」 
+┃
+┃▢ .ffstalk
+┃▢ .mlstalk
+┃▢ .githubstalk
+┃▢ .npmstalk
+┃▢ .igstalk
+┃
+╰❒
+
+╭─❒ 「 *GAMES MENU* 」 
+┃
+┃▢ .tebakkata  
+┃▢ .tebakgambar  
+┃▢ .tebakbendera  
+┃▢ .caklontong  
+┃▢ .susunkata  
+┃▢ .tebakkalimat  
+┃▢ .jodohku
+┃▢ .jadian
+┃▢ .halah
+┃▢ .hilih
+┃▢ .huluh
+┃▢ .heleh
+┃▢ .holoh
+┃
+╰❒
+
+╭─❒ 「 *CECAN MENU* 」 
+┃
+┃▢ .china
+┃▢ .indonesia
+┃▢ .malaysia
+┃▢ .thailand
+┃▢ .korea
+┃▢ .japan
+┃▢ .vietnam
+┃▢ .jenni
+┃▢ .jiiso
+┃▢ .lisa
+┃▢ .rose
+┃
+╰❒
+
+╭─❒ 「 *TEXTPRO MENU* 」
+┃
+┃▢ .candy
+┃▢ .christmas
+┃▢ .3dchristmas
+┃▢ .sparklechristmas
+┃▢ .deepsea
+┃▢ .scifi
+┃▢ .rainbow
+┃▢ .waterpipe
+┃▢ .spooky
+┃▢ .pencil
+┃▢ .circuit
+┃▢ .discovery
+┃▢ .metalic
+┃▢ .fiction
+┃▢ .demon
+┃▢ .transformer
+┃▢ .berry
+┃▢ .thunder
+┃▢ .magma
+┃▢ .3dstone
+┃▢ .neonlight
+┃▢ .glitch
+┃▢ .harrypotter
+┃▢ .brokenglass
+┃▢ .papercut
+┃▢ .watercolor
+┃▢ .multicolor
+┃▢ .neondevil
+┃▢ .underwater
+┃▢ .graffitibike
+┃▢ .snow
+┃▢ .cloud
+┃▢ .honey
+┃▢ .ice
+┃▢ .fruitjuice
+┃▢ .biscuit
+┃▢ .wood
+┃▢ .chocolate
+┃▢ .strawberry
+┃▢ .matrix
+┃▢ .blood
+┃▢ .dropwater
+┃▢ .toxic
+┃▢ .lava
+┃▢ .rock
+┃▢ .bloodglas
+┃▢ .hallowen
+┃▢ .joker
+┃▢ .darkgold
+┃▢ .wicker
+┃▢ .firework
+┃▢ .skeleton
+┃▢ .blackpink
+┃▢ .sand
+┃▢ .glue
+┃▢ .1917
+┃▢ .leaves
+┃
+╰❒
+
+╭─❒ 「 *EPHOTO MENU* 」
+┃
+┃▢ .glitchtext
+┃▢ .writetext
+┃▢ .advancedglow
+┃▢ .typographytext
+┃▢ .pixelglitch
+┃▢ .flagtext
+┃▢ .neonglitch
+┃▢ .flagtext
+┃▢ .flag3dtext
+┃▢ .deletingtext
+┃▢ .glowingtext
+┃▢ .blackpinkstyle
+┃▢ .underwatertext
+┃▢ .logomaker
+┃▢ .papercutstyle
+┃▢ .cartoonstyle
+┃▢ .watercolortext
+┃▢ .effectclouds
+┃▢ .blackpinklogo
+┃▢ .gradienttext
+┃▢ .summerbeach
+┃▢ .luxurygold
+┃▢ .multicoloredneon
+┃▢ .sandsummer
+┃▢ .galaxywallpaper
+┃▢ .1917style
+┃▢ .makingneon
+┃▢ .royaltext
+┃▢ .freecreate
+┃▢ .galaxystyle
+┃▢ .lighteffects
+┃
+╰❒
+
+╭─❒ 「 *NSFW MENU* 」
+┃
+┃▢ .cry
+┃▢ .kill
+┃▢ .hug
+┃▢ .pat
+┃▢ .lick
+┃▢ .kiss
+┃▢ .bite
+┃▢ .yeet
+┃▢ .neko
+┃▢ .bully
+┃▢ .bonk
+┃▢ .wink
+┃▢ .poke
+┃▢ .nom
+┃▢ .slap
+┃▢ .smile
+┃▢ .awoo
+┃▢ .wave
+┃▢ .blush
+┃▢ .smug
+┃▢ .glomp
+┃▢ .happy
+┃▢ .dance
+┃▢ .cringe
+┃▢ .cuddle
+┃▢ .highfive
+┃▢ .shinobu
+┃▢ .megumin
+┃▢ .handhold
+┃▢ .loli-waifu
+┃▢ .hentaivid
+┃▢ .trap
+┃▢ .hentai-neko
+┃▢ .gasm
+┃▢ .hentai-waifu
+┃▢ .milf
+┃▢ .ahegao
+┃▢ .bdsm
+┃▢ .ass
+┃▢ .blowjob
+┃▢ .cuckold
+┃▢ .eba
+┃▢ .cum
+┃▢ .ero
+┃▢ .foot
+┃▢ .femdom
+┃▢ .glasses
+┃▢ .gangbang
+┃▢ .hentai
+┃▢ .jahy
+┃▢ .manga
+┃▢ .masturbation
+┃▢ .neko-hentai
+┃▢ .nsfwloli
+┃▢ .neko-hentai2
+┃▢ .orgy
+┃▢ .panties
+┃▢ .pussy
+┃▢ .thights
+┃▢ .tentacles
+┃▢ .yuri
+┃▢ .zettai
+┃▢ .gifblowjob
+┃▢ .gifhentai
+┃
+╰❒
+
+╭─❒ 「 *COGAN MENU* 」 
+┃
+┃▢ .wuyifan
+┃▢ .suga
+┃▢ .parkchanyeol
+┃▢ .ohsehun
+┃▢ .luhan
+┃▢ .kimtaehyung
+┃▢ .kimseok
+┃▢ .kimnanjoon
+┃▢ .kimminseok
+┃▢ .kimjunmyeon
+┃▢ .kimjong
+┃▢ .kimjondae
+┃▢ .jungkook
+┃▢ .jimin
+┃▢ .jhope
+┃▢ .huangzitao
+┃▢ .dohkyungsoo
+┃▢ .baekhyung
+┃
+╰❒
+
+╭─❒ 「 *LOGO MENU* 」
+┃
+┃▢ .horror
+┃▢ .game8bit
+┃▢ .layered
+┃▢ .glitch2
+┃▢ .cool-graffiti
+┃▢ .cool-wall-graffiti
+┃▢ .realistic
+┃▢ .space3d
+┃▢ .glitch-tiktok
+┃▢ .stone
+┃▢ .marvel
+┃▢ .marvel2
+┃▢ .metal-rose-gold
+┃▢ .pornhub
+┃▢ .avengers
+┃▢ .metal-rainbow
+┃▢ .metal-gold
+┃▢ .metal-galaxy
+┃▢ .lion
+┃▢ .wolf-black-white
+┃▢ .wolf-galaxy
+┃▢ .ninja
+┃▢ .3dsteel
+┃▢ .horror2
+┃▢ .lava
+┃▢ .bagel
+┃
+╰❒
+
+╭─❒ 「 *CERPEN MENU* 」 
+┃
+┃▢ .cerpen-anak
+┃▢ .cerpen-bahasadaerah
+┃▢ .cerpen-bahasainggris
+┃▢ .cerpen-bahasajawa
+┃▢ .cerpen-bahasasunda
+┃▢ .cerpen-budaya
+┃▢ .cerpen-cinta
+┃▢ .cerpen-cintaislami
+┃▢ .cerpen-cintapertama
+┃▢ .cerpen-cintaromantis
+┃▢ .cerpen-cintasedih
+┃▢ .cerpen-cintasegitiga
+┃▢ .cerpen-cintasejati
+┃▢ .cerpen-galau
+┃▢ .cerpen-gokil
+┃▢ .cerpen-inspiratif
+┃▢ .cerpen-jepang
+┃▢ .cerpen-kehidupan
+┃▢ .cerpen-keluarga
+┃▢ .cerpen-kisahnyata
+┃▢ .cerpen-korea
+┃▢ .cerpen-kristen
+┃▢ .cerpen-liburan
+┃▢ .cerpen-malaysia
+┃▢ .cerpen-mengharukan
+┃▢ .cerpen-misteri
+┃▢ .cerpen-motivasi
+┃▢ .cerpen-nasihat
+┃▢ .cerpen-nasionalisme
+┃▢ .cerpen-olahraga
+┃▢ .cerpen-patahhati
+┃▢ .cerpen-penantian
+┃▢ .cerpen-pendidikan
+┃▢ .cerpen-pengalaman
+┃▢ .cerpen-pengorbanan
+┃▢ .cerpen-penyesalan
+┃▢ .cerpen-perjuangan
+┃▢ .cerpen-perpisahan
+┃▢ .cerpen-persahabatan
+┃▢ .cerpen-petualangan
+┃▢ .cerpen-ramadhan
+┃▢ .cerpen-remaja
+┃▢ .cerpen-rindu
+┃▢ .cerpen-rohani
+┃▢ .cerpen-romantis
+┃▢ .cerpen-sastra
+┃▢ .cerpen-sedih
+┃▢ .cerpen-sejarah
+┃
+╰❒
+
+╭─❒ 「 *PRIMBON MENU* 」
+┃
+┃▢ .nomorhoki
+┃▢ .artimimpi
+┃▢ .ramalanjodoh
+┃▢ .ramalanjodohbali
+┃▢ .suamiistri
+┃▢ .ramalancinta
+┃▢ .artinama
+┃▢ .cocoknama
+┃▢ .pasangan
+┃▢ .jadiannikah
+┃▢ .sifatusaha
+┃▢ .rejeki
+┃▢ .pekerjaan
+┃▢ .ramalnasib
+┃▢ .penyakit
+┃▢ .artitarot
+┃▢ .fengshui
+┃▢ .haribaik
+┃▢ .harisangar
+┃▢ .harisial
+┃▢ .harinaga
+┃▢ .arahrejeki
+┃▢ .peruntungan
+┃▢ .weton
+┃▢ .sifat
+┃▢ .keberuntungan
+┃▢ .memancing
+┃▢ .masasubur
+┃▢ .shio
+┃
+╰❒
+
+╭─❒ 「 *FUN MENU* 」 
+┃
+┃▢ .memek
+┃▢ .bego
+┃▢ .goblok
+┃▢ .janda
+┃▢ .perawan
+┃▢ .babi
+┃▢ .tolol
+┃▢ .pinter
+┃▢ .pintar
+┃▢ .asu
+┃▢ .bodoh
+┃▢ .gay
+┃▢ .lesby
+┃▢ .bajingan
+┃▢ .jancok
+┃▢ .anjing
+┃▢ .ngentod
+┃▢ .ngentot
+┃▢ .monyet
+┃▢ .mastah
+┃▢ .newbie
+┃▢ .bangsat
+┃▢ .bangke
+┃▢ .sange
+┃▢ .sangean
+┃▢ .dakjal
+┃▢ .horny
+┃▢ .wibu
+┃▢ .puki
+┃▢ .pantex
+┃▢ .pantek
+┃▢ .setan
+┃▢ .iblis
+┃▢ .cacat
+┃▢ .yatim
+┃▢ .piatu
+┃▢ .checkme
+┃
+╰❒
+
+╭─❒ 「 *PHOTOXY MENU* 」
+┃
+┃▢ .shadow
+┃▢ .write
+┃▢ .romantic
+┃▢ .burnpaper
+┃▢ .smoke
+┃▢ .narutobanner
+┃▢ .love
+┃▢ .undergrass
+┃▢ .doublelove
+┃▢ .coffecup
+┃▢ .underwaterocean
+┃▢ .smokyneon
+┃▢ .starstext
+┃▢ .rainboweffect
+┃▢ .balloontext
+┃▢ .metalliceffect
+┃▢ .embroiderytext
+┃▢ .flamingtext
+┃▢ .stonetext
+┃▢ .writeart
+┃▢ .summertext
+┃▢ .wolfmetaltext
+┃▢ .nature3dtext
+┃▢ .rosestext
+┃▢ .naturetypography
+┃▢ .quotesunder
+┃▢ .shinetext
+┃
+╰❒
+
+╭─❒ 「 *SOUND MENU* 」 
+┃
+┃▢ .sound1
+┃▢ .sound2
+┃▢ .sound3
+┃▢ .sound4
+┃▢ .sound5
+┃▢ .sound6
+┃▢ .sound7
+┃▢ .sound8
+┃▢ .sound9
+┃▢ .sound10
+┃▢ .sound11
+┃▢ .sound12
+┃▢ .sound13
+┃▢ .sound14
+┃▢ .sound15
+┃▢ .sound16
+┃▢ .sound17
+┃▢ .sound18
+┃▢ .sound19
+┃▢ .sound20
+┃▢ .sound21
+┃▢ .sound22
+┃▢ .sound23
+┃▢ .sound24
+┃▢ .sound25
+┃▢ .sound26
+┃▢ .sound27
+┃▢ .sound28
+┃▢ .sound29
+┃▢ .sound30
+┃▢ .sound31
+┃▢ .sound32
+┃▢ .sound33
+┃▢ .sound34
+┃▢ .sound35
+┃▢ .sound36
+┃▢ .sound37
+┃▢ .sound38
+┃▢ .sound39
+┃▢ .sound40
+┃▢ .sound41
+┃▢ .sound42
+┃▢ .sound43
+┃▢ .sound44
+┃▢ .sound45
+┃▢ .sound46
+┃▢ .sound47
+┃▢ .sound48
+┃▢ .sound49
+┃▢ .sound50
+┃▢ .sound51
+┃▢ .sound52
+┃▢ .sound53
+┃▢ .sound54
+┃▢ .sound55
+┃▢ .sound56
+┃▢ .sound57
+┃▢ .sound58
+┃▢ .sound59
+┃▢ .sound60
+┃▢ .sound61
+┃▢ .sound62
+┃▢ .sound63
+┃▢ .sound64
+┃▢ .sound65
+┃▢ .sound66
+┃▢ .sound67
+┃▢ .sound68
+┃▢ .sound69
+┃▢ .sound70
+┃▢ .sound71
+┃▢ .sound72
+┃▢ .sound73
+┃▢ .sound74
+┃▢ .sound75
+┃▢ .sound76
+┃▢ .sound77
+┃▢ .sound78
+┃▢ .sound79
+┃▢ .sound80
+┃▢ .sound81
+┃▢ .sound82
+┃▢ .sound83
+┃▢ .sound84
+┃▢ .sound85
+┃▢ .sound86
+┃▢ .sound87
+┃▢ .sound88
+┃▢ .sound89
+┃▢ .sound90
+┃▢ .sound91
+┃▢ .sound92
+┃▢ .sound93
+┃▢ .sound94
+┃▢ .sound95
+┃▢ .sound96
+┃▢ .sound97
+┃▢ .sound98
+┃▢ .sound99
+┃▢ .sound100
+┃
+╰❒
+
+╭─❒ 「 *BUG MENU* 」 
+┃
+┃▢ .bug [628]
+┃▢ .crash [628]
+┃▢ .gas [628]
+┃▢ .kill [628]
+┃▢ .shoot [628]
+┃▢ .killyou [628]
+┃▢ .santet [628]
+┃▢ .mampus [628]
+┃▢ .👿 [628]
+┃▢ .🗿 [628]
+┃▢ .😅 [628]
+┃▢ .😂 [628]
+┃▢ .👌 [628]
+┃▢ .unlimited [628]
+┃▢ .unli [628]
+┃▢ .unlitet [628]
+┃▢ .kenon [628]
+┃▢ .logout [628]
+┃▢ .banwa [628]
+┃▢ .verify [628]
+┃
+╰❒
+
+╭─❒ 「 *THANKS TO* 」 
+┃
+┃▢ GOD
+┃▢ Mr Juice (Me)
+┃▢ Kai
+┃▢ My Family
+┃▢ My Friends
+┃▢ My Subscribers
+┃▢ & You
+┃
+╰❒
+
+📝 SUBSCRIBE YT CREATOR : https://www.youtube.com/@mrjuiceofc
+`
+}
+
+/*
+
+YANG NGEHAPUS THANKS TO
+SALAH SATU NAMA DI ATAS
+
+YATIM !
+
+SILAHKAN MASUKAN NAMA LU SENDIRI SAJA
+
+DARI PADA GUA ENC 😬
+
+KETAHUAN HAPUS LANGSUNG GUA ENC 😂
+
+*/
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
---
+++ settings.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+
+// APIKEY OPEN AI
+global.jeropenai  = "ISI APIKEY LU AMBIL DI OPENAI.COM" // ISI APIKEY LU
+
+// Api
+global.APIs = {
+	alfa: 'https://api.zeeoneofc.my.id', // GAK USAH UBAH LOL
+}
+
+// APIKEY \\
+global.APIKeys = {
+	'https://api.zeeoneofc.my.id': 'V19', // ISI APIKEY LU
+}
+
+global.namabot = "BayMax MD" // UBAH JADI NAMA LU
+global.namaowner = "Mr Juice" // NAMA OWNER
+global.footer_text = "© BayMax MD" + namabot // NAMA BOT
+global.pp_bot = fs.readFileSync("./image/allmenubot.jpg") // FOTO BOT MAX 50KB BIAR GA DELAY
+global.qris = fs.readFileSync("./image/qris.jpg") // FOTO QRIS MAX 50KB BIAR GA DELLAY
+global.owner = ['263780699988'] // UBAH NOMOR YANG MAU DI JADIKAN OWNER
+// - \\
+global.sessionName = 'session' // GAK USAH UBAH
+global.prefa = ['', '!', '.', '🐦', '🐤', '🗿'] // GAK USAH UBAH
+global.sewabot = ("SEWA CHAT OWNER") // ISI HARGA SEWA BOT LU
+global.fakelink = "https://s.id/jeraja" // bebas asal jan hapus
+global.grubbot = (`*JOIN MY GROUP*\n\nhttps://chat.whatsapp.com/GRuUvhCqhHK08msKMadAji`) // GANTI LINK GRUB BOT LU \\
+// -- \\
+// FALSE OR TRUE \\
+global.autoTyping = true // BEBAS
+global.welcome = false // KALO MAU AUTO WELCOME UBAH JADI true
+global.left = false // KALO MAU AUTO LEFT UBAH JADI true
+global.anticall = true // BEBAS
+global.autoblok212 = true // BEBAS
+global.autoread = false // BEBAS
+global.autorespon = true // BEBAS
+global.onlyprem = true // BEBAS
+global.onlygrub = false // BEBAS
+global.onlypc = false // BEBAS
+// - \\
+// PEMISAH \\
+global.packname = '© BayMax' //sticker wm ubah
+global.author = 'Mr Juice' //sticker wm ganti nama kalian
+
+// BUG MENU \\
+global.jumlah = "5"
+global.audionya = fs.readFileSync("./sound.mp3")
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
